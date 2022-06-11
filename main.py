1d8389549cf94b1c2cdd0944b28af748e14f3fec
Exo <lennyj2468+la@gmail.com>
2022-06-11 12:03:40 +0100

    Add tag format

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ class MyClient(discord.Client):
         print(f'Logged on as {self.user}!')
     
     async def on_message(self, message):
+
         def is_me(message):
             return message.author == self.user
-        if message.author == self.user:
+        if is_me(message):
             return
         #if message.channel.id != 980786397696888883:
         #    print(f'Message `{message.content}` from `{message.author}` in {message.channel.id} was not in the correct channel!')
@@ -19,10 +20,14 @@ class MyClient(discord.Client):
                 await message.delete()
                 await message.author.send(f'Please use the correct format for your message.')
             thread_name = message.content.split('\n')[0].split('Title: ')[1]
-            await message.channel.purge(limit=100, check=is_me)
+            await message.channel.purge(limit=10, check=is_me)
             sent_message = await message.channel.send('Use this format for your message: \n Title: \n Description:')
             await sent_message.pin()
             await message.channel.create_thread(name=f"[{message.author.name}] {thread_name}", message=message)
+        if message.channel.id == 985136865789218866:
+            if not message.content.startswith('id: '):
+                await message.delete()
+                await message.author.send(f'Tag format: \n id: <id> \n out: <out>')
     async def on_member_join(self, member):
         channel = self.get_channel(952598947069841480)
         await channel.send(f'Welcome {member.mention}! Please read the <#952600261023649802> and have fun!')

970f7bdc9eb167bab68550cd42a262933cde73a6
Exo <lennyj2468+la@gmail.com>
2022-06-24 10:24:38 +0100

    Add await

--- main.py
+++ main.py
@@ -72,6 +72,7 @@ class MyClient(discord.Client):
                         member = message.guild.get_member(id_to_try)
                         if member is None:
                             member = await client.fetch_user(id_to_try)
+                            print(member)
                             print(member.display_name)
                         print(member.name)
                     else:

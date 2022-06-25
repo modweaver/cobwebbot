d38c9ad4ce56c53cacf60f48f6f92ce03b89b9bd
ExoPlant <lennyj2468+la@gmail.com>
2022-06-25 19:32:34 +0100

    Push stuff

--- CobwebBot/Program.cs
+++ CobwebBot/Program.cs
@@ -8,6 +8,7 @@ using DSharpPlus.Entities;
 using DSharpPlus.CommandsNext;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using CobwebBot.Handlers;
 
 namespace CobwebBot
 {
@@ -56,129 +57,15 @@ namespace CobwebBot
                 {
                     await e.Message.RespondAsync("pong!");
                 }
+                if (e.Message.Channel.Name == "support")
+                {
+                    SupportChannelHandler.Handle(s, e);
+                }
             };
 
             discord.ComponentInteractionCreated += async (s, e) =>
             {
-                await e.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
-                var user = e.User;
-                var member = await e.Guild.GetMemberAsync(user.Id);
-                DiscordRole role;
-                var guildId = e.Guild.Id;
-
-
-                if (e.Id == "modweaver_role_give") 
-                {
-                    if (guildId == 843489593319751682) // test server
-                    {
-                        role = e.Guild.GetRole(990313379153473586);
-                        var userRoles = member.Roles;
-                        if (userRoles.Contains(role))
-                        {
-                            await member.RevokeRoleAsync(role);
-                        }
-                        else
-                        {
-                            await member.GrantRoleAsync(role);
-                        }
-                    } else if (guildId == 952598946511986699) // real server
-                    {
-                        role = e.Guild.GetRole(990314173416226816);
-                        var userRoles = member.Roles;
-                        if (userRoles.Contains(role))
-                        {
-                            await member.RevokeRoleAsync(role);
-                        }
-                        else
-                        {
-                            await member.GrantRoleAsync(role);
-                        }
-                    }
-                } else if (e.Id == "cobwebapi_role_give")
-                {
-                    if (guildId == 843489593319751682) // test server
-                    {
-                        role = e.Guild.GetRole(990313429078265877);
-                        var userRoles = member.Roles;
-                        if (userRoles.Contains(role))
-                        {
-                            await member.RevokeRoleAsync(role);
-                        }
-                        else
-                        {
-                            await member.GrantRoleAsync(role);
-                        }
-                    }
-                    else if (guildId == 952598946511986699) // real server
-                    {
-                        role = e.Guild.GetRole(990314228198027274);
-                        var userRoles = member.Roles;
-                        if (userRoles.Contains(role))
-                        {
-                            await member.RevokeRoleAsync(role);
-                        }
-                        else
-                        {
-                            await member.GrantRoleAsync(role);
-                        }
-                    }
-                } else if (e.Id == "cwlauncher_role_give")
-                {
-                    if (guildId == 843489593319751682) // test server
-                    {
-                        role = e.Guild.GetRole(990313457163337778);
-                        var userRoles = member.Roles;
-                        if (userRoles.Contains(role))
-                        {
-                            await member.RevokeRoleAsync(role);
-                        }
-                        else
-                        {
-                            await member.GrantRoleAsync(role);
-                        }
-                    }
-                    else if (guildId == 952598946511986699) // real server
-                    {
-                        role = e.Guild.GetRole(990314259537870928);
-                        var userRoles = member.Roles;
-                        if (userRoles.Contains(role))
-                        {
-                            await member.RevokeRoleAsync(role);
-                        }
-                        else
-                        {
-                            await member.GrantRoleAsync(role);
-                        }
-                    }
-                } else if (e.Id == "webcrawler_role_give")
-                {
-                    if (guildId == 843489593319751682) // test server
-                    {
-                        role = e.Guild.GetRole(990313520593788988);
-                        var userRoles = member.Roles;
-                        if (userRoles.Contains(role))
-                        {
-                            await member.RevokeRoleAsync(role);
-                        }
-                        else
-                        {
-                            await member.GrantRoleAsync(role);
-                        }
-                    }
-                    else if (guildId == 952598946511986699) // real server
-                    {
-                        role = e.Guild.GetRole(990314322934784090);
-                        var userRoles = member.Roles;
-                        if (userRoles.Contains(role))
-                        {
-                            await member.RevokeRoleAsync(role);
-                        }
-                        else
-                        {
-                            await member.GrantRoleAsync(role);
-                        }
-                    }
-                }             
+                InteractionHandler.Handle(s, e);
             };
             commands.RegisterCommands<Commands.Utilities>();
             commands.RegisterCommands<Commands.Moderation>();

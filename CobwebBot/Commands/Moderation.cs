28540d5efb6b548e92009fc3f84b1229a014b182
JФSԐPЊҤЇ ҠЯДԞФЩSҜЇ <kizz4114@yahoo.com>
2022-06-26 02:14:09 +0930

    " It's a suffix not a prefix" - Ecorous

--- CobwebBot/Commands/Moderation.cs
+++ CobwebBot/Commands/Moderation.cs
@@ -31,7 +31,7 @@ namespace CobwebBot.Commands
         public async Task MuteCommand(CommandContext ctx, DiscordMember member, string duration, string reason)
         {
             var tDuration = duration;
-            string durationPrefix = "";
+            string durationSuffix = "";
             bool isHours = false, isMinutes = false, isSeconds = false;
             if (duration.EndsWith("h"))
             {
@@ -39,31 +39,31 @@ namespace CobwebBot.Commands
                 duration = duration.Split("h")[0];
                 if (duration.Equals("1h"))
                 {
-                    durationPrefix = "hour";
+                    durationSuffix = "hour";
                 }
                 else
                 {
-                    durationPrefix = "hours";
+                    durationSuffix = "hours";
                 }
             }
             else if (duration.EndsWith("m"))
             {
                 isMinutes = true;
                 duration = duration.Split("m")[0];
-                durationPrefix = duration.Equals("1m") ? "minute" : "minutes";
+                durationSuffix = duration.Equals("1m") ? "minute" : "minutes";
             }
             else if (duration.EndsWith("s"))
             {
                 isSeconds = true;
                 duration = duration.Split("s")[0];
-                durationPrefix = duration.Equals("1s") ? "second" : "seconds";
+                durationSuffix = duration.Equals("1s") ? "second" : "seconds";
             }
             else
             {
                 await ctx.RespondAsync("Invalid duration! See !help for more info!");
                 return;
             }
-            string EmbedDescription = "You have been muted in " + ctx.Guild.Name + "\n \n Reason: " + reason + "\n \n Duration: " + duration + " " + durationPrefix;
+            string EmbedDescription = "You have been muted in " + ctx.Guild.Name + "\n \n Reason: " + reason + "\n \n Duration: " + duration + " " + durationSuffix;
             DiscordEmbed Embed = new DiscordEmbedBuilder().WithTitle("Muted!").WithDescription(EmbedDescription).WithAuthor("Moderator: " + ctx.Message.Author.Username + "#" + ctx.Message.Author.Discriminator).WithColor(DiscordColor.Yellow);
             DateTimeOffset time = new DateTimeOffset(DateTime.Now.AddMinutes(5));
             if (isHours)
@@ -87,7 +87,7 @@ namespace CobwebBot.Commands
 
             await member.SendMessageAsync(Embed);
             await member.TimeoutAsync(time, reason);
-            await ctx.Message.RespondAsync("Muted " + member.Mention + " for " + duration + " " + durationPrefix + "!");
+            await ctx.Message.RespondAsync("Muted " + member.Mention + " for " + duration + " " + durationSuffix + "!");
         }
     }
 }

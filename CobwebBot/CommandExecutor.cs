fd42ee389752d5c4a7a5a19e0738c3d69a98cb5b
Exo <lennyj2468+la@gmail.com>
2022-08-02 05:14:59 +0100

    buggy stuff everything explode boom boom v0

--- CobwebBot/CommandExecutor.cs
+++ CobwebBot/CommandExecutor.cs
@@ -52,7 +52,7 @@ public sealed class CommandExecutor
         System.Console.WriteLine(thingd);
         // Remove the mention...
         string fullCommand = eventArgs.Message.Content.Split(" ")[0].Split("!")[1];
-
+        Console.WriteLine(fullCommand);
         // See if the message is an actual command...
         Command? command = commandsNext.FindCommand(fullCommand, out string? arguments);
         

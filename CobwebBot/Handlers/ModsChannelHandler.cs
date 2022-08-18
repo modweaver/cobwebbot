b22a8de95e128b44e67377041d23b81d2ac7bb13
Exo <lennyj2468+la@gmail.com>
2022-08-18 18:54:58 +0100

    h

--- CobwebBot/Handlers/ModsChannelHandler.cs
+++ CobwebBot/Handlers/ModsChannelHandler.cs
@@ -9,7 +9,7 @@ namespace CobwebBot.Handlers
         public async static void Handle(DiscordClient s, MessageCreateEventArgs e)
         {
             DiscordMember member = (DiscordMember)e.Message.Author;
-            if (e.Message.Attachments.Count != 0 || e.Message.Content.Split("https:").Length > 1)
+            if (e.Message.Attachments.Count < 0 || e.Message.Content.Split("https:").Length < 1)
             {
                 await member.SendMessageAsync("Please make a thread to discuss mods. If you want to post a mod, make sure there is a link to the mod or the mod file is uploaded.");
             }

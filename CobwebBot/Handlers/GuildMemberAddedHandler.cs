b058233a30dbc83270782afd7748e215dadcc737
Exo <lennyj2468+la@gmail.com>
2022-08-26 20:42:50 +0100

    Update new member handling

--- CobwebBot/Handlers/GuildMemberAddedHandler.cs
+++ CobwebBot/Handlers/GuildMemberAddedHandler.cs
@@ -1,4 +1,5 @@
 using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 
 namespace CobwebBot.Handlers;
@@ -9,7 +10,17 @@ public class GuildMemberAddedHandler
     public async static void Handle(DiscordClient s, GuildMemberAddEventArgs e)
     {
         var channel = e.Guild.GetDefaultChannel();
+        var members_ = e.Guild.Members;
+        Dictionary<ulong, DiscordMember?> new_members = new();
+        foreach (var member in members_)
+        {
+            if (!member.Value.IsBot)
+            {
+                new_members.Add(member.Key, member.Value);
+            }
+        }
+        
         await channel.SendMessageAsync(
-            $"Welcome {e.Member.Mention}! If you're here to download mods, visit <#966085042621251614> - this channel is for posting mods you have made and downloading from. Or you can visit https://modweaver.org, a site created by us. If you need help with anything, feel free to post a message in <#980789224217403422>");
+            $"Welcome {e.Member.Mention}, you're member #{new_members.Count}! If you're here to download mods, visit <#966085042621251614> - this channel is for posting mods you have made and downloading from. Or you can visit https://modweaver.org, a site created by us. If you need help with anything, feel free to post a message in <#980789224217403422>");
     }
 }
